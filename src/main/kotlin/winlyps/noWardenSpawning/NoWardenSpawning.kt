eb0bf64889d802f121db46855c5ab461bb9906ea
Winlypse <nts3@o2.pl>
2024-10-22 15:31:35 +0200

    Changes!

--- src/main/kotlin/winlyps/noWardenSpawning/NoWardenSpawning.kt
+++ src/main/kotlin/winlyps/noWardenSpawning/NoWardenSpawning.kt
@@ -6,9 +6,14 @@ class NoWardenSpawning : JavaPlugin() {
 
     override fun onEnable() {
         // Plugin startup logic
+        server.worlds.forEach { world ->
+            world.setGameRuleValue("doWardenSpawning", "false")
+        }
+        logger.info("NoWardenSpawning plugin has been enabled.")
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
+        logger.info("NoWardenSpawning plugin has been disabled.")
     }
 }
