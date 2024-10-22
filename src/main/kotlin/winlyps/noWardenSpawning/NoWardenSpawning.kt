b9eae512a3e2135b8d3983a04447d96a7894e1a5
Winlypse <nts3@o2.pl>
2024-10-22 15:31:23 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/noWardenSpawning/NoWardenSpawning.kt
@@ -0,0 +1,14 @@
+package winlyps.noWardenSpawning
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NoWardenSpawning : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
